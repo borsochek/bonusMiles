8f0dd22f4a88b3ae6b78e90edb1d90684542dca5
borsochek <idievafaina@gmail.com>
2023-10-06 13:26:24 +0300

    change

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
 
-    public int calculate(int a) {
-        int result = (a / 20) * 1;
+    public int calculate(int money) {
+        int result = (money / 20) * 1;
 
         return result;
     }
